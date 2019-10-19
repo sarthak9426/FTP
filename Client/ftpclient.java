876bbb14411d5685aaaea1e3441a573403dbc7b9
sarthak9426 <sarthak9426@gmail.com>
2019-10-19 02:32:15 -0400

    Client and Server java classes

---
+++ Client/ftpclient.java
@@ -0,0 +1,158 @@
+import java.net.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.util.*;
+
+public class ftpclient {
+	Socket requestSocket; // socket connect to the server
+	ObjectOutputStream out; // stream write to the socket
+	ObjectInputStream in; // stream read from the socket
+	int portNo;
+	Boolean cflag = Boolean.FALSE;//flag to check if the client is already connected or not 
+ 	Boolean lflag = Boolean.FALSE; // flag to break out of the while loop if login is successfull
+
+	public ftpclient() {
+	}
+
+	void run() {
+		try {
+			while(true) {
+				BufferedReader Reader = new BufferedReader(new InputStreamReader(System.in));
+				System.out.println("Please enter a command: ");
+				String raw_cmd = Reader.readLine(); // command from input
+				String cmd = raw_cmd.toLowerCase();
+				if ( cmd.startsWith("ftpclient") ) {
+					if ( cflag==true ) {
+						System.out.println("You are already connected to the server. Please try a different command");
+					} else {
+						if (cmd.equalsIgnoreCase("ftpclient localhost 8000")) {
+							requestSocket = new Socket("localhost", 8000);
+							System.out.println("Connected to server at localhost with port 8000");
+							cflag = Boolean.TRUE;
+							out = new ObjectOutputStream(requestSocket.getOutputStream());
+							out.flush();
+							in = new ObjectInputStream(requestSocket.getInputStream());
+							while ( lflag == false ) {
+								System.out.println("Please enter your username:");
+								String us = Reader.readLine();
+								sendMessage(us);
+								System.out.println("Please enter your password:");
+								String pa = Reader.readLine();
+								sendMessage(pa);
+								String temp = (String) in.readObject();
+								System.out.println(temp);
+								if (temp.equalsIgnoreCase("Logged in successfully")) {
+									//update flag to get out of the loop
+									lflag = Boolean.TRUE;
+								}
+							}
+						} else 
+							System.out.println("IP or Port No Invalid. Please try again with valid IP and Port No. ");
+					}
+
+				} else if (cmd.startsWith("get")) {
+					if (cflag) {
+						Boolean flag = Boolean.FALSE;
+						out.writeObject(cmd);
+						flag = in.readBoolean();
+						if ( flag==true ) {	
+							byte[] bytearr = new byte[8192];
+							String filename = cmd.substring(cmd.indexOf(' ') + 1);
+							FileOutputStream fostream = new FileOutputStream(filename);
+							in.read(bytearr, 0, bytearr.length);
+							String s = new String(bytearr);
+							bytearr = s.trim().getBytes();
+							fostream.write(bytearr, 0, bytearr.length);
+							fostream.close();
+							System.out.println("File " + filename + " received successfully");
+						
+						} else 
+							System.out.println("No such file exists. Please try again with a valid file name.");
+
+					} else 
+						System.out.println("No connection found. Please connect to the server first");
+
+				} else if (cmd.startsWith("upload")) {
+					if (cflag==true) {
+						out.writeObject(cmd);
+						String filename = cmd.substring(cmd.indexOf(' ') + 1);
+						byte[] bytearr = new byte[8192];
+						File file = new File(filename);
+						if (file.exists()) {
+							out.writeBoolean(Boolean.TRUE);
+							FileInputStream finputs = new FileInputStream(filename);
+							bytearr=finputs.readAllBytes();
+							out.write(bytearr,0,bytearr.length);
+							System.out.println("Sending file... " + filename);
+							finputs.close();
+							out.flush();
+							System.out.println("Success.");
+						} else 
+						{
+							out.writeBoolean(Boolean.FALSE);
+							System.out.println("Invalid file name. Please try again");
+						}
+					} else
+						System.out.println("No connection found. Please connect to the server first");
+
+				} else if (cmd.equalsIgnoreCase("dir")) {
+					if (cflag) {
+						out.writeObject(cmd);
+						Object obj = in.readObject();
+						if (obj instanceof ArrayList<?>) {
+							ArrayList<?> filenames = (ArrayList<?>) obj;
+							if (filenames.size() > 0) {
+								for (int i = 0; i < filenames.size(); i++) {
+									Object o = filenames.get(i);
+									if (o instanceof String) {
+										System.out.println("\t" + filenames.get(i));
+									}
+								}
+							}
+
+						}
+					} else 
+						System.out.println("No connection found. Please connect to the server first");
+				} else 
+					System.out.println("Command Invalid. Please enter a valid command.");
+			}
+
+		} catch (ConnectException e) {
+			System.err.println("Connection refused. You need to initiate a server first.");
+		} catch (ClassNotFoundException e) {
+			System.err.println("Class not found");
+		} catch (UnknownHostException unknownHost) {
+			System.err.println("You are trying to connect to an unknown host!");
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		} finally {
+			// Close connections
+			try {
+				in.close();
+				out.close();
+				requestSocket.close();
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+	}
+
+	// send a message to the output stream
+	void sendMessage(String msg) {
+		try {
+			// stream write the message
+			out.writeObject(msg);
+			out.flush();
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
+
+	// main method
+	public static void main(String args[]) {
+		ftpclient client = new ftpclient();
+		client.run();
+	}
+
+}
---
+++ Server/ftpserver.java
@@ -0,0 +1,163 @@
+import java.net.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.util.*;
+
+public class ftpserver {
+
+	private static final int sPort = 8000;   //The server will be listening on this port number
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("The server is running."); 
+    	ServerSocket listener = new ServerSocket(sPort);
+		int clientNum = 1;
+        	try {
+            		while(true) {
+                		new Handler(listener.accept(),clientNum).start();
+				System.out.println("Client "  + clientNum + " is connected!");
+				clientNum++;
+            			}
+        	} finally {
+            		listener.close();
+        	} 
+ 
+    	}
+
+	/**
+     	* A handler thread class.  Handlers are spawned from the listening
+     	* loop and are responsible for dealing with a single client's requests.
+     	*/
+		private static class Handler extends Thread {
+    	private String cmd;    //message received from the client
+		private Socket connection;
+    	private ObjectInputStream in;	//stream read from the socket
+    	private ObjectOutputStream out;    //stream write to the socket
+		private int no;		//The index number of the client
+		private Boolean lflag; // flag to keep checking if client is logged in or not
+		
+    	public Handler(Socket connection, int no) {
+    		this.connection = connection;
+    		this.no = no;
+    	}
+
+        public void run() {
+ 		try{
+ 			
+ 			lflag = Boolean.FALSE;
+			//initialize input and output streams
+			out = new ObjectOutputStream(connection.getOutputStream());
+			out.flush();
+			in = new ObjectInputStream(connection.getInputStream());
+			try{
+				while(true)
+				{
+					while( lflag==false )
+					{
+						//receive the message sent from the client
+						String us = (String)in.readObject();
+						System.out.println("Got username: " + us + " from client " + no);
+						String pa = (String)in.readObject();
+						if( us.equalsIgnoreCase("admin") && pa.equalsIgnoreCase("123") ) 
+						{
+							lflag = Boolean.TRUE;
+							sendMessage("Logged in successfully");
+						}
+						else
+						{
+							sendMessage("Login unsucessfull...Retry");
+						}
+					}
+					cmd = (String)in.readObject();
+					System.out.println("Received command: " + cmd + " from client " + no);					
+					if(cmd.startsWith("get"))
+					{
+						String fname= cmd.substring( cmd.indexOf(' ') + 1 );
+						File file = new File(fname);
+						if(file.exists())
+						{
+							out.writeBoolean(Boolean.TRUE);
+							byte [] bytearr  = new byte [8192];
+							FileInputStream finputs = new FileInputStream(fname);
+					        bytearr = finputs.readAllBytes();
+					        System.out.println("Sending " + fname+ ".....");
+					        out.write(bytearr,0,bytearr.length);
+					        System.out.println("Success");
+					        // Close the fileinput stream
+					        finputs.close(); 
+							out.flush();
+						}
+						else
+						{
+							out.writeBoolean(Boolean.FALSE);
+							out.flush();
+						}	
+					}
+					else if(cmd.startsWith("upload"))
+					{
+						Boolean flag = Boolean.FALSE;
+						String fname= cmd.substring(cmd.indexOf(' ') + 1);
+						flag=in.readBoolean();
+					    if(flag) {
+					    	byte [] bytearr  = new byte [8192];
+							FileOutputStream fostream = new FileOutputStream(fname);
+						    int count;
+					    	while(( count = in.read(bytearr))>0 ){
+					    		fostream.write(bytearr,0,count);
+					    	}
+					    	System.out.println("File " + fname + " received");
+					    	// Close the fileoutput stream
+					    	fostream.close();
+					    }
+					    else
+					    	System.out.println("Error in UPLOAD - Please try again with valid filename");
+					}
+					else if(cmd.equalsIgnoreCase("dir"))
+					{
+						String dir = System.getProperty("user.dir");
+						File f = new File(dir);
+						File[] list = f.listFiles();
+						int n=list.length;
+						ArrayList<String> fnames =new ArrayList<String>();
+						for (int i = 0; i < n; i++) {
+							if (list[i].isFile()) {
+								fnames.add(list[i].getName());
+							} 
+						}
+						out.writeObject(fnames);
+					}
+				}
+			}
+			catch(ClassNotFoundException classnot){
+					System.err.println("Input in in unknown format");
+				}
+		}
+		catch(IOException ioException){
+			System.out.println("Disconnecting with client " + no);
+		}
+		finally{
+			//Close connections
+			try{
+				in.close();
+				out.close();
+				connection.close();
+			}
+			catch(IOException ioException){
+				System.out.println("Disconnect with client " + no);
+			}
+		}
+	}
+
+	public void sendMessage(String msg)
+	{
+		try{
+			out.writeObject(msg);
+			out.flush();
+			System.out.println( msg + " with Client " + no);
+		}
+		catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+	}
+    }
+}
